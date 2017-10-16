25f83254e7a55edf23d948d5215b9d61aa56004a
Paige Israel <paigeaisrael@gmail.com>
2017-10-16 02:08:45 -0700

    Cropping pictures, creating essays-books.php
    1. Cropping pictures
    2. Adding them to essays-books.php
    3. Adjusting style sheets

--- pages/essays-books.php
+++ pages/essays-books.php
@@ -5,22 +5,23 @@
 
 	<div class="page-wdth100pct">
 		
-		<div class="header-center-bold">
+		<div class="header-center-bold bottom-1pct">
             <span>E</span>ssays &#38; <span>C</span>ollections</a>
         </div>
 
-        <div class="essay-series-summary-wdth100pct">
-            <div class="small-header">
-                <a href="shards.php">Shards of a Broken Mirror:<br></a>
-                <a href="shards.php"><span class="italic smaller">&#40;Songs&#44; Poems&#44; &#38; Thoughts &#47; Journals&#41;</span></a>
-            </div>
-
-            <!-- start of shards of a broken mirror -->
+        <!-- start of shards of a broken mirror -->
+        <div class="essay-series-summary">
             <div class="series-pic-iftmus">
-                <a href="songs-poems-thoughts.php"><img src="../images/F-shards-400x241.jpg" width="400" height="241" alt="Shards of a broken mirror image" /></a>
+                <a href="songs-poems-thoughts.php"><img src="../images/shards-250x256.jpg" width="250" height="253" alt="Shards of a Broken Mirror Image" /></a>
             </div>
 
-            <div class="series-text-adventure-list">
+            <!-- <div class="small-header-essay">
+                <a href="shards.php">Shards of a Broken Mirror:</a><br>
+                <a href="shards.php"><span class="italic smaller">&#40;Songs&#44; Poems&#44; &#38; Thoughts &#47; Journals&#41;</span></a>
+            </div> -->
+
+
+            <!-- <div class="series-text-adventure-list">
                 Section I&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large"><a href="songs.php">Songs</a></span><br>
                 Section II&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large"><a href="poems.php">Poems</a></span><br>
                 Section III&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="large"><a href="thoughts.php">Thoughts&#32;/&#32;Journals</a></span><br>
@@ -34,24 +35,68 @@
                 Burns quietly within us all&#46;&#46;&#46;<br>
                 And our shackled spirits desperately yearn<br>
                 If only briefly&#44; to touch a part of Him&#46;<br>
-            </div>
-        </div>
+            </div>-->
+        </div> 
 
-            <!-- start of true lies -->
+        <!-- start of eulogy through my grandfather -->
 
-        <div class="essay-series-summary-wdth100pct"> 
-            <div class="small-header">
-                <a href="under-construction.php">True Lies<br></a>
-            </div>
+        <div class="essay-series-summary wdth70pct"> 
+            <!-- <div class="small-header-essay">
+                <a href="under-construction.php">Eulogy Through My Grandfather</a>
+            </div> -->
 
             <div class="series-pic-iftmus">
-                <a href="under-construction.php"><img src="../images/true-lies-400x425.jpg" width="408" height="333" alt="True Lies image" /></a>
+                <a href="under-construction.php"><img src="../images/eulogy-250x208.jpg" width="250" height="208" alt="Eulogy Through My Grandfather Image" /></a>
             </div>
-            <div class="essay-text-excerpt">
+            <!-- <div class="essay-text-excerpt">
                 Whether trying to recall an event or cover the actions within&#44;<br>
                 there is only one way to recall the past&#46;
+            </div> -->
+        </div>
+
+        <!-- start of random thoughts -->
+
+        <div class="essay-series-summary wdth70pct"> 
+            <div class="series-pic-iftmus">
+                <a href="under-construction.php"><img src="../images/random-250x202.jpg" width="250" height="202" alt="Random Thoughts Image" /></a>
             </div>
         </div>
+
+        <!-- start of dreams -->
+
+        <div class="essay-series-summary wdth70pct"> 
+            <div class="series-pic-iftmus">
+                <a href="under-construction.php"><img src="../images/dreams-250x216.jpg" width="250" height="216" alt="Dreams Image" /></a>
+            </div>
+        </div>
+
+        <!-- start of true lies -->
+
+        <div class="essay-series-summary wdth70pct"> 
+            <!-- <div class="small-header-essay">
+                <a href="under-construction.php">True Lies<br></a>
+            </div> -->
+
+            <div class="series-pic-iftmus">
+                <a href="under-construction.php"><img src="../images/true-lies-250x278.jpg" width="250" height="278" alt="True Lies Image" /></a>
+            </div>
+            <!-- <div class="essay-text-excerpt">
+                Whether trying to recall an event or cover the actions within&#44;<br>
+                there is only one way to recall the past&#46;
+            </div> -->
+        </div>
+
+        <!-- start of I havd dreamed of many things -->
+
+        <div class="essay-series-summary wdth70pct"> 
+            <div class="series-pic-iftmus">
+                <a href="under-construction.php"><img src="../images/many-things-250x211.jpg" width="250" height="211" alt="I Have Dreamed of Many Things Image" /></a>
+            </div>
+        </div>
+
+        
+        
+
 
 
 
