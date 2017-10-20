8d5da1696a1b4016b0b536206adb6aa96bbd66f1
Paige Israel <paigeaisrael@gmail.com>
2017-10-20 03:03:06 -0700

    Fixed iftmus.php FINALLY!
    1. Fixed prlogue section of accordion
    2. Uploaded scripts with jquery ui
    3. Updated styles1.php

--- pages/iftmus.php
+++ pages/iftmus.php
@@ -1360,10 +1360,10 @@
 				</div>  <!-- end Prologues div -->
             </div>    
         </div> <!-- end content class div -->
-		  </div> <!-- end content id div -->
+		  </div> <!-- end accrdn-prlog id div -->
 		  
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
 
 		<!-- jQuery UI library -->
 		<script src="../scripts/jquery-ui/jquery-ui.min.js"></script>
