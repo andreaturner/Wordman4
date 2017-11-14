067db7241247a1ce943a94b0fcbf58ecf0b23d6d
Paige Israel <paigeaisrael@gmail.com>
2017-11-14 15:44:59 -0800

    Re-did text in about page
    1. Changed text on about.php

--- pages/about.php
+++ pages/about.php
@@ -2,58 +2,83 @@
     $page_title = 'Biography &amp; Published Works';
     include ('../shared/header.html');
 ?>    
-    <div class="content">
-		<div class="header-center-bold">
-			<span>B</span>iography
-		</div>
-        
-        <div class="series-pic-about-top">
-            <img src="../images/eric-color-head-shot-319x400.jpg" width="319" height="400" alt="Eric Color Head Shot Image" />
-        </div>
+    
 
-        <div class="bio">
-            <span class="larger vivaldi">Eric Koch</span> <span class="large">&#40;Cook&#41;</span> 
-            <br>
 
-            <div class="margin-bottom-1">
-                <span>M</span>y parents were offspring of Russian/German immigrants; a tough crowd to play. I grew up in the shelter of the 50&#39;s
-                and slow realizations of the 60&#39;s. Like all of us we think our time was the best. I was timid and unsure but my heritage demanded I
-                pick my lot in life and pursue it with a passion.
-            </div> 
+    <div class="page-wdth100pct">
 
-            <div class="margin-bottom-1">
-                <span>I</span> have been writing stories since grade school. A few of my books were even confiscated in high school with a warning and 
-                a half smile; I suppose I should have had the decency to blush.
-            </div>    
+        <div class="content">
+    		<div class="header-center-bold">
+    			<span>B</span>iography
+    		</div>
             
-            <div class="margin-bottom-1"">
-                <span>D</span>uring that time I discovered another outlet that intrigued me. I studied the words and music of the folk &amp; rock genre. I taught myself guitar by removing all the strings and putting them back on, two at a time. Discerning which combination of fret positions sounded
-                pleasant, I would then add two more strings. When done, I had finally formed chords; no clue as to what they were called until I started interacting
-                with other musicians. From then on I was writing my own original music. It was a rewarding time when people cared and listened. I played local bars and made a fair living.
+            <div class="series-pic-about-top">
+                <img src="../images/eric-color-head-shot-275x345.jpg" width="275" height="345" alt="Eric Color Head Shot Image" />
             </div>
 
-            <div class="margin-bottom-1">
-                <span>I</span> am currently a published, Performing / Songwriter / Poet, and have composed over 1,140 songs. After what seems a lifetime of performing my music across the country, up and down the West Coast, inward to the Midwest and Southwest and finally ending up on the East Coast, I am once again home; residing in
-                the Pacific Northwest.
-            </div>
+                <div class="bio-box">
 
-            <div class="margin-bottom-1">
-                <span>I</span> continued writing stories for years, I treated it as an ongoing exercise; something to do when I wasn&#39;t creating or performing my music. I am now winding down the traveling/performing aspect of music while recording my own songs in my home studio; smooth jazz instrumental versions of my varied compositions. I've since redirected my focus on my writings and my passion for photography.
-            </div>
+                    <div class="bio">
+                        <span class="larger vivaldi">Eric Koch</span> <span class="large">&#40;Cook&#41;</span> 
+                        <br>
 
-            <div class="margin-bottom-1">
-                <span>B</span>ack on the road, while playing music in Summit, New Jersey, I was lucky enough to meet a woman who rekindled hope and inspiration to my heart; this is a good thing since, by then, I was a sputtering candle. She tells me that she is my souvenir from the East Coast and while I have to agree, I include Lesley as a force in this final endeavor in my life; she always refused to allow me to accept anything less than my best which she is&#33;
-            </div>
+                        <div class="margin-bottom-1">
+                            <span>B</span>orn and raised in the Seattle &#47; Tacoma area of Washington State&#44; my parents were offspring of Russian &#47; German immigrants&#59; a tough crowd to play&#46; 
+                            I grew up in the shelter of the 50&#39;s and slow realizations of the 60&#39;s. Like all of us we think our time was the best&#46; As a child I was a bit timid&#44; if not unsure&#44; 
+                            but my heritage demanded I pick my lot in life and pursue it with a passion&#46; That choice was always easy&#46;
+                        </div> 
 
-        </div>  <!-- [closes bio div] -->
+                        <div class="margin-bottom-1">
+                            <span>I</span> had been writing stories since grade school&#46; A few of my books were even confiscated in high school with a warning and a half smile&#59; I suppose I should have 
+                            had the decency to blush&#46;
+                        </div>    
+                        
+                        <div class="margin-bottom-1"">
+                            <span>D</span>uring that time I discovered another outlet that intrigued me&#46; I studied the words and music of the folk &#38; rock genre&#46; I taught myself guitar by removing 
+                            all the strings and putting them back on&#44; two at a time&#46; Discerning which combination of fret positions sounded pleasant&#44; I would then add two more string&#46; When done&#44; 
+                            I had finally formed chords&#59; no clue as to what they were called until I started interacting with other musicians&#46; From then on I was writing my own original music&#46; 
+                            It was a rewarding time when people cared and listened&#46; I played local bars&#44; making an erratic if not unstable living&#46;
+                        </div>
+
+                        <div class="margin-bottom-1">
+                            <span>I</span> am currently a published&#44; Performing &#47; Songwriter &#47; Poet&#44; and have composed over 1&#44;140 songs&#46;. While playing music in New Jersey&#44; I was 
+                            lucky enough to meet a woman who rekindled hope and inspiration to my heart&#59; this is a good thing since&#44; by then I was a flickering candle&#46; She tells me that she is 
+                            my souvenir from the East Coast and while I have to agree&#44; I include Lesley as a force in these final chapters in my life. In all endeavors she always refused to allow me 
+                            to accept anything less than my best which she most certainly is&#46;
+                        </div>
+
+                        <div class="margin-bottom-1">
+                            <span>A</span>fter what seems a lifetime of performing my music across the country&344; up and down the West Coast&344; inward to the Mid and Southwest and finally on the East Coast&344; 
+                            I am once again home&#59; residing in the Pacific Northwest&#46;
+                        </div>
+
+                        <div class="margin-bottom-1">
+                            <span>I</span> have continued writing stories over the years&#59; treating it as an ongoing exercise&344; when I wasn*#39;t creating or performing my music&346; I have now abated the 
+                            traveling &347; performing aspect of music and am recording my original songs, adapting them into smooth jazz instrumental versions&346; I've since redirected my focus on my writings 
+                            and my passion for photography&346; I have two unique sons&344; five wonderful grandchildren and three loyal dogs&346; Life is always good if not busy&346;
+                        </div>
+
+                    </div>  <!-- [closes bio div] -->
+
+                        <div class="series-pic-about-bottom">
+                            <img src="../images/wedding-200x265.jpg" width="200" height="265" alt="Eric and Leslie Koch Image" />
+                        </div>
+
+                </div>  <!--[closes bio-box div] -->    
+
+                        
+
+        </div>  <!-- [closes page-wdth100pct div] -->
 
             <!-- <div class="entry-footer">
                 <span class="divider-line"></span>
             </div> -->
-        
-		<div class="series-pic-about-bottom">
-			<img src="../images/wedding-302x400.jpg" width="302" height="400" alt="Eric and Leslie Koch Image" />
-		</div>
+                    
+            		
+
+
+
+		
 	</div>  <!-- [end content div] -->
 		
 		<!-- [start credits] -->
@@ -69,10 +94,13 @@
             <dl>
                 <dt>&#8220;High County Fair&#8221; <span class="smaller">&#169;&#44; 1973&#46;</span></dt>
                     <dd>Song placed &#35;2 in the CBS Television National Talent Search&#46;</dd>
+                    <br>
                 <dt>Founded &#8220;The Pacific Northwest Songwriter's Showcase,&#8221; 1974 &#8211; 1976&#46;</dt>
                     <dd>Created and wrote the definitions and by-laws for my non-profit Organization to promote aspiring songwriters before I turned it over to its members.</dd>
+                    <br>
                 <dt>&#8220;The Tree Song&#8221; <span class="smaller">&#169;&#44; 1975&#46;</span></dt>
                     <dd>Song placed Top 10&#37; in the SRS International Songwriting Contest, allowing me to book my live performances through the William Morris Agency.</dd>
+                    <br>
                 <dt>And There Isn&#39;t Any Magic Here Today <span class="smaller">&#169;, 1977&#46; </span><span>Album</span></dt>
                     <dd>&#8220;Keep On Dancin&#39;&#8221;</dd>
                     <dd>&#8220;I Never Really Cared Too Much &#40;For Being Single&#41;&#8221;</dd>
@@ -84,22 +112,28 @@
                     <dd>&#8220;Prayer&#8221;</dd>
                     <dd>&#8220;And There Isn&#39;t Any Magic Here Today&#8221;</dd>
                     <dd>&#8220;The Savior&#8221;</dd>
+                    <br>
                 <dt>BMI Cash Advance, &#8220;Songwriting Excellence Award&#8221; <span class="smaller">&#169;&#44; 1980&#46;</span></dt>
                     <dd>&#8220;It Ain&#39;t Fair&#8221;</dd>
                     <dd>&#8220;Babysittin&#39; Lover&#8221;</dd>
                     <dd>&#8220;A Question of Love&#8221;</dd>
                     <dd>&#8220;Castles in the Sand&#8221;</dd>
                     <dd>&#8220;Love Has Always Been That Way&#8221;</dd>
+                    <br>
                 <dt>Eulogy through my Grandfather  <span class="smaller">&#169;&#44; 1982&#46; </span><span>Essay</span></dt>
                     <dd>I was asked by Hospice, pastors and a funeral facility for permission to use, read or reprint it&#46;</dd>
+                    <br>
                 <dt>&#8220;Shut Up &#38; Dance&#8221; <span class="smaller">&#169;&#44; 1984&#46;</span></dt>
                     <dd>Song written for the Pizza Time Chain and its monthly teen dances&#46;</dd>
+                    <br>
                 <dt>&#8220;Stand Up and Take on Tomorrow&#8221; <span class="smaller">&#169;&#44; 1986&#46;</span></dt>
                     <dd>Song written for The New Jersey Center for Cancer Survivors&#46;</dd>
+                    <br>
                 <dt>Cold Iron  <span class="smaller">&#169;&#44; 1996&#46;</span></dt>
-                    <dd>Poem written for Sundown M Ranch Rehabilitation Center and its counselors&#46;</dd>
+                    <dd>Poem written for Sundown M Ranch Rehabilitation Center and its counselors&#46;</dd><br>
                 <dt>Even When The Angels Dream  <span class="smaller">&#169;&#44; 1998&#46;</span></dt>
-                    <dd>Poem published in the book &#8220;After The First Snowfall&#8221;
+                    <dd>Poem published in the book &#8220;After The First Snowfall&#8221;</dd>
+                    <br>    
                 <dt>The Saga of Sir Choc&#45;o&#45;lot <span class="smaller">&#169;&#44; 1999&#46;</span></dt>
                     <dd>Musical Play written for the Seattle Chocolate Festival&#59; to raise money for the Nortwest Harvest Food Bank&#46;</dd>
                     <dd><span><strong>Songs&#58;</strong></span></dd>
@@ -107,6 +141,7 @@
                     <dd>&#8220;Cause It Ain&#39;t No Fun&#8221;</dd>
                     <dd>&#8220;Why Not&#8221;</dd>
                     <dd>&#8220;Sweeter than Candy &#40;Second To None&#41;&#8221;</dd>
+                    <br>
                 <dt>You&#39;re An Animal  <span class="smaller">&#169;&#44; 2000&#46;</span></dt>
                     <dd>Theme Song for the television show &#8220;Animal Action&#8221; on The Animal Planet&#44; aired on the Fox Network for Kids&#46;
             </dl>
