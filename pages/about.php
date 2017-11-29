228bba5fce294223fe926d81e0eaaa54e909841c
Paige Israel <paigeaisrael@gmail.com>
2017-11-29 14:16:12 -0800

    Old files restored
    1. about.php restored
    2. styles3.css restored

--- pages/about.php
+++ pages/about.php
@@ -4,81 +4,57 @@
 ?>    
     
 
+
     <div class="page-wdth100pct">
 
         <div class="content">
     		<div class="header-center-bold">
     			<span>B</span>iography
     		</div>
-
-
             
-            
+            <div class="series-pic-about-top">
+                <img src="../images/eric-color-head-shot-275x345.jpg" width="275" height="345" alt="Eric Color Head Shot Image" />
+            </div>
 
-           
                 <div class="bio-box">
-
-                    <div class="image-box">
-                        <div class="series-pic-about-top">
-                            <img src="../images/eric-color-head-shot-275x345.jpg" width="275" height="345" alt="Eric Color Head Shot Image" />
-                        </div>
-
-                        <div class="series-pic-about-middle">
-                            <img src="../images/josh-chris-275x187.jpg" width="275" height="187" alt="Josh and Chris Image" />
-                        </div>
-                    </div>
 
                     <div class="bio">
                         <span class="larger vivaldi">Eric Koch</span> <span class="large">&#40;Cook&#41;</span> 
                         <br>
 
                         <div class="margin-bottom-1">
-                            <span>B</span>orn and raised in the Seattle &#47; Tacoma area of Washington State&#44; my parents were offspring of Russian &#47; German immigrants&#59; a tough crowd to play&#46; 
-                            I grew up in the shelter of the 50&#39;s and slow realizations of the 60&#39;s. Like all of us we think our time was the best&#46; As a child I was a bit timid&#44; if not unsure&#44; 
-                            but my heritage demanded I pick my lot in life and pursue it with a passion&#46; That choice was always easy&#46;
+                            Born and raised in the Seattle / Tacoma area of Washington State, my parents were offspring of Russian / German immigrants; a tough crowd to play. I grew up in the shelter of the 50's and slow realizations of the 60's. Like all of us we think our time was the best. As a child I was a bit timid, if not unsure, but my heritage demanded I pick my lot in life and pursue it with a passion. That choice was always easy.
                         </div> 
 
                         <div class="margin-bottom-1">
-                            <span>I</span> had been writing stories since grade school&#46; A few of my books were even confiscated in high school with a warning and a half smile&#59; I suppose I should have 
-                            had the decency to blush&#46;
+                            <span>I</span> have been writing stories since grade school. A few of my books were even confiscated in high school with a warning and 
+                            a half smile; I suppose I should have had the decency to blush.
                         </div>    
                         
                         <div class="margin-bottom-1"">
-                            <span>D</span>uring that time I discovered another outlet that intrigued me&#46; I studied the words and music of the folk &#38; rock genre&#46; I taught myself guitar by removing 
-                            all the strings and putting them back on&#44; two at a time&#46; Discerning which combination of fret positions sounded pleasant&#44; I would then add two more string&#46; When done&#44; 
-                            I had finally formed chords&#59; no clue as to what they were called until I started interacting with other musicians&#46; From then on I was writing my own original music&#46; 
-                            It was a rewarding time when people cared and listened&#46; I played local bars&#44; making an erratic if not unstable living&#46;
+                            <span>D</span>uring that time I discovered another outlet that intrigued me. I studied the words and music of the folk &amp; rock genre. I taught myself guitar by removing all the strings and putting them back on, two at a time. Discerning which combination of fret positions sounded
+                            pleasant, I would then add two more strings. When done, I had finally formed chords; no clue as to what they were called until I started interacting
+                            with other musicians. From then on I was writing my own original music. It was a rewarding time when people cared and listened. I played local bars and made a fair living.
                         </div>
 
                         <div class="margin-bottom-1">
-                            <span>I</span> am currently a published&#44; Performing &#47; Songwriter &#47; Poet&#44; and have composed over 1&#44;140 songs&#46;. While playing music in New Jersey&#44; I was 
-                            lucky enough to meet a woman who rekindled hope and inspiration to my heart&#59; this is a good thing since&#44; by then I was a flickering candle&#46; She tells me that she is 
-                            my souvenir from the East Coast and while I have to agree&#44; I include Lesley as a force in these final chapters in my life. In all endeavors she always refused to allow me 
-                            to accept anything less than my best which she most certainly is&#46;
+                            <span>I</span> am currently a published, Performing / Songwriter / Poet, and have composed over 1,140 songs. After what seems a lifetime of performing my music across the country, up and down the West Coast, inward to the Midwest and Southwest and finally ending up on the East Coast, I am once again home; residing in
+                            the Pacific Northwest.
                         </div>
 
                         <div class="margin-bottom-1">
-                            <span>A</span>fter what seems a lifetime of performing my music across the country&#44; up and down the West Coast&#44; inward to the Mid and Southwest and finally on the East Coast&#44; 
-                            I am once again home&#59; residing in the Pacific Northwest&#46;
+                            <span>I</span> continued writing stories for years, I treated it as an ongoing exercise; something to do when I wasn&#39;t creating or performing my music. I am now winding down the traveling/performing aspect of music while recording my own songs in my home studio; smooth jazz instrumental versions of my varied compositions. I've since redirected my focus on my writings and my passion for photography.
                         </div>
 
                         <div class="margin-bottom-1">
-                            <span>I</span> have continued writing stories over the years&#59; treating it as an ongoing exercise&#44; when I wasn&#39;t creating or performing my music&#46; I have now abated the 
-                            traveling &#47; performing aspect of music and am recording my original songs, adapting them into smooth jazz instrumental versions&#46; I've since redirected my focus on my writings 
-                            and my passion for photography&#46; I have two unique sons&#44; five wonderful grandchildren and three loyal dogs&#46; Life is always good if not busy&#46;
+                            <span>B</span>ack on the road, while playing music in Summit, New Jersey, I was lucky enough to meet a woman who rekindled hope and inspiration to my heart; this is a good thing since, by then, I was a sputtering candle. She tells me that she is my souvenir from the East Coast and while I have to agree, I include Lesley as a force in this final endeavor in my life; she always refused to allow me to accept anything less than my best which she is&#33;
                         </div>
 
                     </div>  <!-- [closes bio div] -->
 
-                        
-
-
                         <div class="series-pic-about-bottom">
                             <img src="../images/wedding-200x265.jpg" width="200" height="265" alt="Eric and Leslie Koch Image" />
                         </div>
-
-                        
-
 
                 </div>  <!--[closes bio-box div] -->    
 
